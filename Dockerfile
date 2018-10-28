113e113f3e83d3ecd231839befb7955e03d6df0c
Simon Willison <simonw@eventbrite.com>
2018-10-27 22:51:45 -0700

    Recipe for running a fastai model in Docker / Zeit Now

---
+++ Dockerfile
@@ -0,0 +1,22 @@
+FROM python:3.6-slim-stretch
+
+RUN apt update
+RUN apt install -y python3-dev gcc
+
+# Install pytorch and fastai
+RUN pip install torch_nightly -f https://download.pytorch.org/whl/nightly/cpu/torch_nightly.html
+RUN pip install fastai
+
+# Install starlette and uvicorn
+RUN pip install starlette uvicorn python-multipart
+
+ADD cougar.py cougar.py
+ADD usa-inaturalist-cats.pth usa-inaturalist-cats.pth
+
+# Run it once to trigger resnet download
+RUN python cougar.py
+
+EXPOSE 8008
+
+# Start the server
+CMD ["python", "cougar.py", "serve"]
---
+++ cougar.py
@@ -0,0 +1,68 @@
+from starlette.applications import Starlette
+from starlette.responses import JSONResponse, HTMLResponse
+from fastai.vision import (
+    ImageDataBunch,
+    ConvLearner,
+    open_image,
+    get_transforms,
+    models,
+)
+from pathlib import Path
+from io import BytesIO
+import sys
+import uvicorn
+
+app = Starlette()
+
+cat_images_path = Path("/tmp")
+cat_fnames = [
+    "/{}_1.jpg".format(c)
+    for c in [
+        "Bobcat",
+        "Mountain-Lion",
+        "Domestic-Cat",
+        "Western-Bobcat",
+        "Canada-Lynx",
+        "North-American-Mountain-Lion",
+        "Eastern-Bobcat",
+        "Central-American-Ocelot",
+        "Ocelot",
+        "Jaguar",
+    ]
+]
+cat_data = ImageDataBunch.from_name_re(
+    cat_images_path,
+    cat_fnames,
+    r"/([^/]+)_\d+.jpg$",
+    ds_tfms=get_transforms(),
+    size=224,
+)
+cat_learner = ConvLearner(cat_data, models.resnet34)
+
+
+@app.route("/upload", methods=["POST"])
+async def homepage(request):
+    data = await request.form()
+    bytes = await (data["file"].read())
+    img = open_image(BytesIO(bytes))
+    losses = img.predict(cat_learner)
+    prediction = cat_learner.data.classes[losses.argmax()]
+    return JSONResponse({"prediction": prediction})
+
+
+@app.route("/form")
+def form(request):
+    return HTMLResponse(
+        """
+    <form action="/upload" method="post" enctype="multipart/form-data">
+    Select image to upload:
+    <input type="file" name="file">
+    <input type="submit" value="Upload Image">
+    </form>
+    """
+    )
+
+
+if __name__ == "__main__":
+    if "serve" in sys.argv:
+        uvicorn.run(app, host="0.0.0.0", port=8008)
